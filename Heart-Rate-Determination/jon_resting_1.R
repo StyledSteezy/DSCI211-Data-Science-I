e1c203a8d3d54e0ca8616919372f3806c1fa61ca
Christian Tomford <43894065+StyledSteezy@users.noreply.github.com>
2020-03-04 17:59:19 -0500

    Add files via upload

---
+++ Heart-Rate-Determination/jon_resting_1.R
@@ -0,0 +1,24 @@
+library(png)
+library(pracma)
+library(zoo)
+
+n <- length(list.files("frames1/", "frame.*")) # Number of frames
+r <- 30.005392    # frame rate
+ts <- (0:(n-1))/r # time of each index
+
+# Your initialization here
+vect = c()
+for (i in 1:n) {
+  name <- sprintf("frames1/frame%05d.png", i)
+  frame <- readPNG(name)
+  rs <- frame[,,1] # Matrix of red values
+  gs <- frame[,,2] # Matrix of green values
+  bs <- frame[,,3] # Matrix of blue values
+  vect = c(vect, mean(rs))
+  # Per frame computation here
+}
+print(vect)
+plot(vect, type="l")
+smooth <- rollapply(vect, width=13, FUN=mean, align = "center", fill=NA)
+plot(smooth, type = "l")
+length(findpeaks(smooth))
